ff7ca93c5ff3b2836ae7cccf332f3f9040c4128a
Egor Balyshev <frag.trials@gmail.com>
2013-08-05 22:57:25 +0800

    Rearranged things, added some error handling

--- index.js
+++ index.js
@@ -11,47 +11,47 @@ var http = require('http'),
 var proxies = ProxyList(__dirname + '/proxies.txt')
 
 http.createServer(function(request, response) {
-    var sr = StoredRequest(request)
-
-    sr.on('ready', function () {
-        this.requestWith(proxies, function (res) {
-            response.writeHead(res.statusCode, res.headers)
-            res.pipe(response)
+    ProxyRequest(request, response, proxies)
+        .on('error', function (error) {
+            response.writeHead(424, 'Proxying Failed', {'Content-Type': 'text/plain'})
+            response.write(error.toString() + '\n')
+            response.end()
         })
-    })
 }).listen(8080)
 
-function StoredRequest(request) {
-    if (!(this instanceof StoredRequest)) return new StoredRequest(request)
-    var that = this
-    _.extend(this, _.pick(request, 'url', 'method', 'headers'))
-    this.body = new Buffer(0)
-    request.on('data', function (chunk) {
-        that.body = that.body.concat(chunk)
-    })
-    request.on('end', function () {
-        that.emit('ready')
+function ProxyRequest(orig_request, orig_response, proxy_list) {
+    if (!(this instanceof ProxyRequest)) return new ProxyRequest(orig_request, orig_response, proxy_list)
+    this.save_request(orig_request, function (request) {
+        this.proxy_request(request, proxy_list, function (response) {
+            this.pipe_response(response, orig_response)
+        })
     })
 }
-util.inherits(StoredRequest, events.EventEmitter)
+util.inherits(ProxyRequest, events.EventEmitter)
 
-StoredRequest.prototype.requestWith = function (proxies, callback) {
-    var options, request, proxy
-
-    proxy = proxies.fetch()
-
-    options = {
-        host: proxy.ip,
-        port: proxy.port,
-        path: this.url,
-        method: this.method,
-        headers: this.headers,
+ProxyRequest.prototype.save_request = function (request, callback) {
+    var saved_request = {
+        headers: request.headers,
+        method:  request.method,
+        path:    request.url,
+        body:    new Buffer(0),
     }
+    request.on('data', function (chunk) {
+        saved_request.body = saved_request.body.concat(chunk)
+    })
+    request.on('end', _.bind(callback, this, saved_request))
+    request.on('error', _.bind(this.emit, 'error'))
+}
 
-    request = http.request(options, callback)
-    request.write(this.body)
+ProxyRequest.prototype.proxy_request = function (saved_request, proxy_list, callback) {
+    var request, proxy
+    proxy = proxy_list.fetch()
+    request = http.request(_.extend(saved_request, proxy), _.bind(callback, this))
+    request.write(saved_request.body)
     request.end()
 }
 
-
-// 424 Failed Dependency
+ProxyRequest.prototype.pipe_response = function (src_response, dst_response) {
+    dst_response.writeHead(src_response.statusCode, src_response.headers)
+    src_response.pipe(dst_response)
+}
